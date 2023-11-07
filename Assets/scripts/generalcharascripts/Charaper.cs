f1fbcdbf1bbd11db11e1e62d04f49dab548d4736
Rikiwastaken <krafftarnaud@gmail.com>
2023-11-07 17:47:09 +0100

    les HP sont montrés en numéros

--- Assets/scripts/generalcharascripts/Charaper.cs
+++ Assets/scripts/generalcharascripts/Charaper.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using UnityEngine.SocialPlatforms;
 
 public class Charaper : MonoBehaviour
 {
 
-    public TextMeshProUGUI TextUI;
+    public TextMeshProUGUI Textper;
+    public TextMeshProUGUI TextHP;
+    public bool hptxt;
 
     public int percent;
+    public int hp;
 
     void Start()
     {
-        TextUI = GetComponent<TextMeshProUGUI>();
+        
+        if(hptxt)
+        {
+            TextHP = GetComponent<TextMeshProUGUI>();
+        }
+        else
+        {
+            Textper = GetComponent<TextMeshProUGUI>();
+        }
+        
     }
 
     // Update is called once per frame
@@ -24,12 +38,23 @@ public class Charaper : MonoBehaviour
         if (this.CompareTag("P1per"))
         {
             percent = GameObject.FindWithTag("Player1").GetComponent<charavalues>().percent;
+            hp= GameObject.Find("Global values").GetComponent<Globalvalues>().p1HP;
         }
         else
         {
             percent = GameObject.FindWithTag("Player2").GetComponent<charavalues>().percent;
+            hp = GameObject.Find("Global values").GetComponent<Globalvalues>().p2HP;
         }
-        TextUI.text = percent.ToString();
+        if (hptxt)
+        {
+            TextHP.text = hp.ToString();
+        }
+        else
+        {
+            Textper.text = percent.ToString();
+        }
+        
+        
 
     }
 }
--- Assets/scripts/generalcharascripts/charavalues.cs
+++ Assets/scripts/generalcharascripts/charavalues.cs
@@ -14,6 +14,7 @@ public class charavalues : MonoBehaviour
     public bool upb; //bool qui détermine quand on active le upb. Il sert à désactiver le blocage de déplacement verticaux quand sur une plateforme
     public bool touché; //bool qui détermine quand un personnage est touché. Il peut être utilisé pour une attaque qui envoit vers le haut. Il sert à désactiver le blocage de déplacement verticaux quand sur une plateforme
     public bool grabed; //bool qui détermine si un personnage se fait grab, il ne peut alors pas bouger ni attaquer.
+    public int hp;
 
     [Header("variables de clignotement lors d'iframe")]
     public int blinkcnt;
